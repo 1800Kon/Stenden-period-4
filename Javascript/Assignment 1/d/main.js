7dfdbb2fa37b37a7ef2e80d1f47a5779ea346b27
Konstan <55129891+KBG360@users.noreply.github.com>
2021-04-25 18:43:10 +0200

    week 1 js

--- Javascript/Assignment 1/d/main.js
+++ Javascript/Assignment 1/d/main.js
@@ -1,7 +1,13 @@
 function addStuff(){
     var text = document.getElementById('text').value
     var div = document.createElement("div")
-    div.innerHTML = text
-    div.setAttribute("class", "h-20 w-full bg-blue-300 rounded-3xl mb-5")
+    var innerText = document.createElement("p")
+
+    innerText.setAttribute("class", "w-full px-2 mx-auto h-auto text-2xl font-bold tracking-tighter text-black title-font")
+    innerText.setAttribute("contenteditable", "true")
+    innerText.innerHTML = text
+    
+    div.setAttribute("class", "h-auto w-full bg-blue-300 rounded-3xl mb-5 flex justify-center")
+    div.appendChild(innerText)
     document.getElementById("stuff").appendChild(div)
 }
